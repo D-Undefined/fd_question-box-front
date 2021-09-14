5b42306da5b0997570dfdb935242d4f1573f05b5
Kouta-fd <kotafd0124@icloud.com>
2021-09-14 15:50:27 +0900

    914anserpage

---
+++ src/components/Answerbox.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>回答</h1>
+    <div>
+      <input type="text" />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "answer"
+};
+</script>
---
+++ src/pages/Answer.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <Answervbox/>
+  </div>
+</template>
+<script>
+import Questionbox from '../components/Answerbox.vue'
+export default {
+  components: {
+    Questionbox
+  }
+}
+</script>
